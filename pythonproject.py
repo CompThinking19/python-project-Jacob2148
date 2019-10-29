6e20418f734b8cbf5dc163ba56d37095d73e6ef4
Jacob2148 <jtd145@scarletmail.rutgers.edu>
2019-10-29 11:16:38 -0400

    comments on the goal of the assignment
    describing what i plan to insert for the game

---
+++ pythonproject.py
@@ -0,0 +1,3 @@
+#An attempt at creating a text adventure and escape room
+#Plan to have the user/player start in one area and try to make their way through a house for necessary things
+#Also considering to have something equivalent to a code that would allow for the user/player to start with a required items to leave
