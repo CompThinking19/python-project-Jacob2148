6e3491bde01335a4d84ad6e49182090613c3d267
Jacob2148 <jtd145@scarletmail.rutgers.edu>
2019-11-05 10:38:56 -0500

    typo fix again
    only 1 required item to leave

--- pythonproject.py
+++ pythonproject.py
@@ -1,6 +1,6 @@
 #An attempt at creating a text adventure and escape room
 #Plan to have the user/player start in one area and try to make their way through a house for necessary things
-#Also considering to have something equivalent to a code that would allow for the user/player to start with a required items to leave
+#Also considering to have something equivalent to a code that would allow for the user/player to start with a required item to leave
 def(main)
     inventory = ["wallet", "driver license"]
     #starting equipment in the game
