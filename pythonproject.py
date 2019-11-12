b07260047bf4c142a31c1fa225720e93cc17ef0f
Jacob2148 <@SchoolHound2148>
2019-11-12 10:27:29 -0500

    added yes/no capability
    allows user to respond yes or no to prompts

--- pythonproject.py
+++ pythonproject.py
@@ -18,3 +18,24 @@ def(search)
      if nothing_found Raise TypeError("Nothing Found")
      #Type error introduced to let the user know nothing was found
 "inventory" == inventory
+
+#yes and no as answers from the user to accomplish an action or not
+def query_yes_no(question, default="yes")
+    if default is None:
+        prompt = " [yes/no] "
+    elif default == "yes":
+        prompt = " [YES/no] "
+    elif default == "no":
+        prompt = " [yes/NO] "
+    else:
+        raise value error("invalid default answer: 'I can not do that'" % default)
+
+    While True:
+        sys.stdout.write(question + prompt)
+        choice = raw_input().lower()
+        if default is not None and choice == '':
+            return valid[default]
+        elif choice in valid:
+            return valid[choice]
+        else:
+            sys.stdout.write("Respond with 'yes' or 'no'")
